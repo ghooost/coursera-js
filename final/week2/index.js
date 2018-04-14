f956dbab6b5f0aa41bdccdeac3660e4501edf394
ghooost <ghooost@gmail.com>
2018-04-14 19:38:02 +0300

    week2

--- final/week2/index.js
+++ final/week2/index.js
@@ -9,58 +9,78 @@
   function Memoji(oBoard){
     this.oBoard=oBoard;
     this.cards=[];
-    addEventListener("click", this.onClick, true);
+    addEventListener("click", this.onClick.bind(this), true);
     var raw=["🐶","🐹","🐱","🐰","🐻","🐭"];
     var emos=raw.concat(raw);
     var cards=oBoard.querySelectorAll("li");
     for(var cnt=0,m=cards.length;cnt<m;cnt++){
       this.cards.push(new Card(cards[cnt],
-        emos.splice(Math.floor(Math.random()*10000)%emos.length, 1)));
+        emos.splice(Math.floor(Math.random()*10000)%emos.length, 1)[0]));
     };
   }
 
   Memoji.prototype.onClick=function(e){
     e.stopPropagation();
     e.preventDefault();
+    if(e.target.tagName=='LI'){
+      var card=this.cards.filter(ele=>ele.isEleOwner(e.target));
+      if(card.length){
+        if(card[0].isModeEq(Card.modes.closed)){
+          var cardToOpen=card[0];
+          //if there is single card
+          var openedCards=this.cards.filter(ele=>ele.isModeEq(Card.modes.single));
+          if(openedCards.length){
+              //there is a card to compare with
+              var cardToCompare=openedCards[0];
+              if(cardToOpen.isSameEmo(cardToCompare)){
+                cardToOpen.setMode(Card.modes.success);
+                cardToCompare.setMode(Card.modes.success);
+              } else {
+                cardToOpen.setMode(Card.modes.error);
+                cardToCompare.setMode(Card.modes.error);
+              };
+          } else {
+              //there is no cards to compare with
+              cardToOpen.setMode(Card.modes.single);
+              var errorCards=this.cards.filter(ele=>ele.isModeEq(Card.modes.error));
+              for(var cnt=0,m=errorCards.length;cnt<m;cnt++){
+                errorCards[cnt].close();
+                errorCards[cnt].setMode(Card.modes.closed);
+              };
+          }
+          cardToOpen.open();
+        }
+      }
+    }
   }
 
   function Card(obj, emo){
-    this.opened=false;
-    this.ele=
+    this.mode='';
+    this.ele=obj;
+    this.emo=emo;
+    this.ele.dataset.emo=emo;
   }
-
-  var reverseClass="reversed";
-  window.addEventListener("load", function(){
-    var raw=["🐶","🐹","🐱","🐰","🐻","🐭"];
-    var emos=raw.concat(raw);
-    var cards=document.querySelectorAll(".board_items li");
-    for(var cnt=0,m=cards.length;cnt<m;cnt++){
-      cards[cnt].addEventListener("click",cardClickCatcher,false);
-      cards[cnt].dataset.emo=emos.splice(Math.floor(Math.random()*10000)%emos.length, 1);
-    }
-  }, true);
-
-  function rotateFaceUp(oCard){
-      oCard.classList.add(reverseClass);
+  Object.defineProperty(Card, 'modes', {
+    value: {closed:'',single:'single',success:'success',error:'error'},
+    writable: false
+  });
+  Card.prototype.setMode=function(mode){
+    this.mode=mode;
+    this.ele.dataset.mode=this.mode;
   }
-
-  function rotateFaceDown(oCard){
-      oCard.classList.remove(reverseClass);
+  Card.prototype.open=function(){
+    this.ele.classList.add('reversed')
   }
-
-  function isFaceUp(oCard){
-    return oCard.classList.contains(reverseClass);
+  Card.prototype.close=function(){
+    this.ele.classList.remove('reversed')
   }
-
-  function cardClickCatcher(){
-    if(isFaceUp(this)){
-      rotateFaceDown(this);
-    } else {
-      var cards=document.querySelectorAll(".board_items li."+reverseClass);
-      for(var cnt=0,m=cards.length;cnt<m;cnt++){
-        rotateFaceDown(cards[cnt]);
-      };
-      rotateFaceUp(this);
-    };
+  Card.prototype.isSameEmo=function(oCard){
+    return this.emo==oCard.emo;
+  }
+  Card.prototype.isEleOwner=function(ele){
+    return this.ele==ele;
+  }
+  Card.prototype.isModeEq=function(mode){
+    return this.mode==mode;
   }
 }());
